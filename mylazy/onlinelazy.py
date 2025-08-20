13ff48836184b18d70aa5cd2bf7005e1a08dc532
a65651626 <a65651626@163.com>
2025-08-20 23:36:03 +0800

    mylazy

--- mylazy/onlinelazy.py
+++ mylazy/onlinelazy.py
@@ -6,6 +6,13 @@ os.environ["LAZYLLM_SENSENOVA_API_KEY"] = "sk-cacM6j9A69tdMa3ih5Cas4OxssqgWZiX"
 
 import lazyllm
 
-chat = lazyllm.OnlineChatModule(source="sensenova",model='SenseChat-5-1202')
-lazyllm.WebModule(chat, port=range(23466, 23470)).start().wait()
+chat1 = lazyllm.OnlineChatModule(source="sensenova",model="SenseChat-5-1202")
+
+print(chat1("你好，你是DeepSeek吗？"))
+print("===========================")
+chat2 = lazyllm.OnlineMultiModalModule(source="sensenova",model="SenseNova-V6-5-Pro")
+print(chat2("你好，你是DeepSeek吗？"))
+
+
+#lazyllm.WebModule(chat, port=range(23466, 23470)).start().wait()
 
---
+++ mylazy/rag.py
@@ -0,0 +1,21 @@
+import os
+
+# 设置 API 密钥（示例：OpenAI）
+os.environ["LAZYLLM_SENSENOVA_API_KEY"] = "sk-cacM6j9A69tdMa3ih5Cas4OxssqgWZiX"
+
+import lazyllm
+documents = lazyllm.Document(dataset_path="Y:/data_kb")
+
+retriever = lazyllm.Retriever(doc=documents,group_name="CoarseChunk",similarity="bm25_chinese",topk=3, output_format='content', join=' ' )
+retriever.start()
+
+prompt = ('You will act as an AI question-answering assistant and complete a dialogue task.'
+          'In this task, you need to provide your answers based on the given context and questions.')
+
+llm=lazyllm.OnlineChatModule(source="sensenova",model='SenseChat-5-1202').prompt(lazyllm.ChatPrompter(instruction=prompt, extra_keys=['context_str']))
+
+query = input('请输入您的问题\n')
+res = llm({"query": query, "context_str": retriever(query=query)})
+
+print(f'With RAG Answer: {res}')
+
